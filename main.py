80671f2f44848dbbec200f39aa0f027c45a6a5b1
DsML01 <davimelovas61@gmail.com>
2026-01-14 17:41:30 -0300

    Chore: Adding starter file of this api

---
+++ main.py
@@ -0,0 +1,7 @@
+from todo_list_empiretech_back import create_app
+from todo_list_empiretech_back.settings import settings
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=settings.FLASK_PORT, debug=settings.FLASK_DEBUG)
