01ecb1158b9f4146ed08d0af4339152c4331bcbb
DsML01 <davimelovas61@gmail.com>
2026-01-14 16:12:31 -0300

    Chore: Added the task schemas

---
+++ todo_list_empiretech_back/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+from datetime import datetime
+
+class TaskBase(BaseModel):
+    title: str = Field(..., max_length=100)
+    description: Optional[str] = None
+    is_completed: bool = False
+    is_in_progress: bool = False
+    priority: int = Field(1, ge=1, le=5, description="Priority must be between 1 (lowest) and 5 (highest)")
+
+    model_config = ConfigDict(extra="forbid") # Forbid extra fields not defined in the model
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str] = None
+    is_completed: Optional[bool] = None
+    is_in_progress: Optional[bool] = None
+    priority: Optional[int] = Field(None, ge=1, le=5, description="Priority must be between 1 (lowest) and 5 (highest)")
+
+    model_config = ConfigDict(extra="forbid")
+class TaskRead(TaskBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
