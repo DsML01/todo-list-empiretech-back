4a78f19174326a8199f5be9973b80506c57ab0e9
DsML01 <davimelovas61@gmail.com>
2026-01-14 19:41:54 -0300

    Refactor: Applied lint

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from todo_list_empiretech_back.settings import settings
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=settings.FLASK_PORT, debug=settings.FLASK_DEBUG)
+    app.run(
+        host='0.0.0.0', port=settings.FLASK_PORT, debug=settings.FLASK_DEBUG
+    )
