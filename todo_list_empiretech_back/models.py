1dc9abe972e7c5e36de027b3fba151b10e65962c
DsML01 <davimelovas61@gmail.com>
2026-01-14 15:29:45 -0300

    Chore: Task model added

---
+++ todo_list_empiretech_back/models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    is_completed = db.Column(db.Boolean, default=False, nullable=False)
+    is_in_progress = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False) #If do not user lambda, the value is going to be the time when the server starts
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+    priority = db.Column(db.Integer, default=0, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'is_completed': self.is_completed,
+            'is_in_progress': self.is_in_progress,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+            'priority': self.priority 
+        }
